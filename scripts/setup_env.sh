a1a6c5017647cd0f1d9ce6d20137045b359faed2
Matthew Zhao <matthew@fusebit.io>
2022-04-15 20:59:38 -0700

    .

--- scripts/setup_env.sh
+++ scripts/setup_env.sh
@@ -2,15 +2,15 @@
 
 set -ex
 
-apt update -y
+sudo apt update -y
 
 # Dependency: golang, make, awscli
 
-apt install golang cmake awscli -y
+sudo apt install golang cmake awscli -y
 
 # Dependency: docker
 
-apt install -y ca-certificates curl gnupg lsb-release
+sudo apt install -y ca-certificates curl gnupg lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
@@ -18,5 +18,5 @@ echo \
     "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
-apt update
-apt install docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io
